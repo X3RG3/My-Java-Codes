83db17a73ee50230b118ba9424268837f6405585
Mohammad Irfan <irfanayon88@gmail.com>
2025-11-05 21:38:20 +0600

    Delete Hello.java

--- Hello.java
+++
@@ -1,7 +0,0 @@
-import java.lang.*;
-public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hello, Java !! ");
-		System.out.println("This is the Basic structure of Java");
-	}
-}
