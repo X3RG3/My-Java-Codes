76aa2f6321d57e24c521d1b8d0b00f76f8dc1e1e
X3RG3 <irfanayon88@gmail.com>
2025-11-20 00:51:35 +0600

    String

--- Practise/Practice.java
+++ Practise/Practice.java
@@ -1,6 +1,6 @@
 public class Practice {
 	public static void main(String[] args){
-		String s = "KiKoroTumi??";
+		String s = "HEllo world";
 		String s2 = "hello";
 		
 		System.out.println(s.length());
