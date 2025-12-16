eeb9521e205182a42c794bd2534cc1eb53ddd410
Mohammad Irfan <irfanayon88@gmail.com>
2025-12-16 10:31:56 +0600

    Delete Abstraction directory

--- Abstraction/A.java
+++
@@ -1,6 +0,0 @@
-abstract class A {
-    abstract void show();         // abstract method
-    void display() {              // regular method
-        System.out.println("Display");
-    }
-}
--- Abstraction/B.java
+++
@@ -1,6 +0,0 @@
-class B extends A {
-    @Override
-    void show() {
-        System.out.println("Show");
-    }
-}
