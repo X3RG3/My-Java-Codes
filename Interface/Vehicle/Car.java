7c2ed6972c80f50577e5f56da177a3e4d5c32f8f
X3RG3 <irfanayon88@gmail.com>
2025-12-23 22:09:15 +0600

    First Interface Practice

---
+++ Interface/Vehicle/Car.java
@@ -0,0 +1,9 @@
+public class Car implements Vehicle{
+	public void Start(){
+		System.out.println("Car Starts.....");
+	}
+	
+	public void Stop(){
+		System.out.println("Car Stops......");
+	}
+}
---
+++ Interface/Vehicle/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[] args){
+		Vehicle v = new Car();
+		
+		v.Start();
+		v.Stop();
+	}
+}
