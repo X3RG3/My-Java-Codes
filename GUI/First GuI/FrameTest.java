215bee9d4aa3b1ab93ae8a456127136e8bbd2028
X3RG3 <irfanayon88@gmail.com>
2025-12-25 15:48:36 +0600

    gui  test

--- GUI/First GuI/FrameTest.java
+++ GUI/First GuI/FrameTest.java
@@ -1,14 +1,37 @@
+import java.lang.*;
 import javax.swing.*;
 public class FrameTest extends JFrame{
 	JPanel panel;
+	JLabel namelbl,passlbl;
+	JTextField namefld;
+	JPasswordField passfld;
 	
 	public FrameTest(){
 		super("MyFirstGUI");
-		this.setSize(800,450);
+		this.setSize(600,400);
 		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		panel = new JPanel();
 		panel.setLayout(null);
+		
+		namelbl = new JLabel("Name : ");
+		namelbl.setBounds(50,90,300,50);
+		panel.add(namelbl);
+		
+		namefld=new JTextField();
+		namefld.setBounds(150,90,350,50);
+		panel.add(namefld);
+		
+		passlbl=new JLabel("Password : ");
+		passlbl.setBounds(50,150,100,50);
+		panel.add(passlbl);
+		
+		passfld = new JPasswordField();
+		passfld.setBounds(150,150,350,50);
+		passfld.setEchoChar('#');
+		panel.add(passfld);
+		
 		this.add(panel);
 	}
 }
--- GUI/First GuI/Main.java
+++ GUI/First GuI/Main.java
@@ -2,6 +2,7 @@ import java.lang.*;
 public class Main{
 	public static void main(String[] args){
 		FrameTest f1 = new FrameTest();
+		
 		f1.setVisible(true);
 	}
 	
