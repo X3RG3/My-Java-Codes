13640e127dfcf455778f180a8451c3be4f3f5bcf
X3RG3 <irfanayon88@gmail.com>
2026-01-19 22:40:09 +0600

    hehehe222

---
+++ See Here/GiftCard10.java
@@ -0,0 +1,8 @@
+        prbtn2 = new JButton("Buy");
+        prbtn2.setBounds(155,66,90,40);
+        prbtn2.setForeground(Color.WHITE);
+        prbtn2.setBackground(c2);
+        prbtn2.setFocusPainted(false);
+        prbtn2.setFont(f2);
+        prbtn2.addActionListener(this);
+        panel.add(prbtn2);
